9d72f6f714da916e59a1e4b204a1feb123a36c41
Adith <adiths746@gmail.com>
2026-03-22 13:26:11 +0530

    route is solve

--- server/routers/movieRoute.js
+++ server/routers/movieRoute.js
@@ -4,6 +4,10 @@ const movieRoutes = express.Router();
 const { getRecommendedMovies, getAIRecommendations, getMovieId, getTrendingMovies } = require("../controllers/movieController");
 const validateToken = require("../middileware/authMiddileware");
 
+movieRoutes.get("/", (req, res) => {
+  res.send("Movies route working");
+});
+
 movieRoutes.get("/recommended", getRecommendedMovies);
 movieRoutes.get("/ai", validateToken, getAIRecommendations);
 movieRoutes.get("/trending", getTrendingMovies);
