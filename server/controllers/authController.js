84d180a4ced368c6d095a329d3dd51dfbc066a3d
Adith <adiths746@gmail.com>
2026-03-27 21:22:37 +0530

    backed login page update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -238,8 +238,8 @@ const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: 'Lax',
+            secure: true,
+            sameSite: 'None',
         })
 
         res.status(200).json({
